12ea75539fe23a77c391930b310263040dc2d2a2
Johnsoniscoming <tengors888@yahoo.com.tw>
2018-06-20 23:13:24 +0800

    update_20180620

--- WindowsForms_Hyperlinks/Form1.Designer.cs
+++ WindowsForms_Hyperlinks/Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvHyperlinkData = new System.Windows.Forms.DataGridView();
+            this.名稱 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.位置 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReadExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtExcelPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSaveDGV = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHyperlinkData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvHyperlinkData
+            // 
+            this.dgvHyperlinkData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHyperlinkData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.名稱,
+            this.位置});
+            this.dgvHyperlinkData.Location = new System.Drawing.Point(26, 34);
+            this.dgvHyperlinkData.Name = "dgvHyperlinkData";
+            this.dgvHyperlinkData.RowTemplate.Height = 31;
+            this.dgvHyperlinkData.Size = new System.Drawing.Size(1045, 355);
+            this.dgvHyperlinkData.TabIndex = 0;
+            this.dgvHyperlinkData.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgvHyperlinkData_KeyDown);
+            // 
+            // 名稱
+            // 
+            this.名稱.HeaderText = "名稱";
+            this.名稱.MinimumWidth = 10;
+            this.名稱.Name = "名稱";
+            // 
+            // 位置
+            // 
+            this.位置.HeaderText = "位置";
+            this.位置.Name = "位置";
+            // 
+            // btnReadExcel
+            // 
+            this.btnReadExcel.Location = new System.Drawing.Point(895, 478);
+            this.btnReadExcel.Name = "btnReadExcel";
+            this.btnReadExcel.Size = new System.Drawing.Size(162, 60);
+            this.btnReadExcel.TabIndex = 1;
+            this.btnReadExcel.Text = "載入Excel";
+            this.btnReadExcel.UseVisualStyleBackColor = true;
+            this.btnReadExcel.Click += new System.EventHandler(this.btnReadExcel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(32, 417);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            // 
+            // txtExcelPath
+            // 
+            this.txtExcelPath.Location = new System.Drawing.Point(131, 417);
+            this.txtExcelPath.Name = "txtExcelPath";
+            this.txtExcelPath.Size = new System.Drawing.Size(732, 29);
+            this.txtExcelPath.TabIndex = 3;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(895, 416);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(162, 30);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSaveDGV
+            // 
+            this.btnSaveDGV.Location = new System.Drawing.Point(36, 500);
+            this.btnSaveDGV.Name = "btnSaveDGV";
+            this.btnSaveDGV.Size = new System.Drawing.Size(108, 38);
+            this.btnSaveDGV.TabIndex = 5;
+            this.btnSaveDGV.Text = "SaveDGV";
+            this.btnSaveDGV.UseVisualStyleBackColor = true;
+            this.btnSaveDGV.Click += new System.EventHandler(this.btnSaveDGV_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 550);
+            this.Controls.Add(this.btnSaveDGV);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtExcelPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReadExcel);
+            this.Controls.Add(this.dgvHyperlinkData);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHyperlinkData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvHyperlinkData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 名稱;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 位置;
+        private System.Windows.Forms.Button btnReadExcel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtExcelPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSaveDGV;
     }
 }
 
--- WindowsForms_Hyperlinks/Form1.cs
+++ WindowsForms_Hyperlinks/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace WindowsForms_Hyperlinks
 {
@@ -15,6 +18,126 @@ namespace WindowsForms_Hyperlinks
         public Form1()
         {
             InitializeComponent();
+            this.dgvHyperlinkData.DataError += delegate (object sender, DataGridViewDataErrorEventArgs e) { };
+        }
+        //*****Paste data to DataGridView
+        private void dgvHyperlinkData_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Modifiers == Keys.Control) && (e.KeyCode == Keys.V) && (dgvHyperlinkData.CurrentCell != null))//Ctrl+V貼上指令
+            {
+                dgvHyperlinkData[dgvHyperlinkData.CurrentCell.ColumnIndex, dgvHyperlinkData.CurrentCell.RowIndex].Value = Clipboard.GetText();
+            }
+        }
+        //***** Browse excel file to text
+        string excelName = string.Empty;
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "File excel | *.xlsx";
+            DialogResult re = fd.ShowDialog();
+            excelName = fd.SafeFileName;
+            if (re == DialogResult.OK)
+            {
+                string fileName = fd.FileName;
+                txtExcelPath.Text = fileName;
+            }
+        }
+
+        private void btnReadExcel_Click(object sender, EventArgs e)
+        {
+            readExcel(txtExcelPath.Text);
+        }
+        //*****Read data on Excel and write onto datagridview
+        string name, path;
+        public void readExcel(string excelPath)
+        {
+            Excel.Application xlAPP;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            int rCnt;
+            int cCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlAPP = new Excel.Application();
+            xlWorkBook = xlAPP.Workbooks.Open(excelPath, 0, true,5,"","",true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1,0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+            for (rCnt = 1; rCnt <= rw; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= cl; cCnt++){
+                    name = Convert.ToString((range.Cells[rCnt, 1] as Excel.Range).Value2);
+                    path = Convert.ToString((range.Cells[rCnt, 2] as Excel.Range).Value2);
+                }
+                dgvHyperlinkData.Rows.Add(name, path);
+            }
+            xlWorkBook.Close(true, null, null);
+            xlAPP.Quit();
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlAPP);
+        }
+        String FilePathName = Application.StartupPath + "/Datagridview";//***previous data of Datagridview
+        private void btnSaveDGV_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog sfg = new SaveFileDialog();
+            //sfg.FileName = "Datagridview";
+            //sfg.Filter = "Text Document|*.text";
+            //sfg.Title = "Save DatagridView as";
+            //DialogResult result = sfg.ShowDialog();
+            //if (result == DialogResult.OK)
+            //{
+            //MessageBox.Show(FilePathName);
+                TextWriter writer = new StreamWriter(FilePathName);
+                for(int i = 0; i <= dgvHyperlinkData.Rows.Count-2; i++)
+                {
+                    for (int j = 0; j <= dgvHyperlinkData.Columns.Count-1; j++)
+                    {
+                        //writer.Write(('\t' + (dgvHyperlinkData.Rows[i].Cells[j].Value.ToString() + ('\t' + "|"))));
+                        writer.Write(dgvHyperlinkData.Rows[i].Cells[j].Value.ToString()+",");
+                    }
+                    writer.WriteLine("");
+                }
+                writer.Close();
+                //MessageBox.Show("Data save");
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Read data from StartupPath. If there is nothing,then null.
+            //*****Check previous data and load in.
+            if (System.IO.File.Exists(FilePathName))
+            {
+                //MessageBox.Show("I got it");
+                TextReader reader = new StreamReader(FilePathName);
+                //dgvHyperlinkData.Rows[0].Cells[0].Value = 1;
+                //dgvHyperlinkData.Rows[0].Cells[1].Value = 1;
+                //MessageBox.Show(Convert.ToString(lines));
+                string a = reader.ReadToEnd();
+                string[] txtFile = a.Split(',');
+                for (int i = 0; i < txtFile.Length-1; i++)
+                {
+                    //if (i == 0)
+                    //    dgvHyperlinkData.Rows[i].Cells[0].Value = txtFile[i];
+                    //else if (i % 2 == 0)
+                    //    dgvHyperlinkData.Rows[i / 2].Cells[0].Value = txtFile[i];
+                    //else
+                    //{
+                    //    dgvHyperlinkData.Rows[i / 2].Cells[1].Value = txtFile[i];
+                    //    dgvHyperlinkData.Rows.Add(txtFile[i]);
+                    //}
+                    if (i%2==0)
+                        dgvHyperlinkData.Rows.Add(txtFile[i],txtFile[i+1]);
+                }                
+            }
+            //else
+            //    MessageBox.Show("I did get it");
         }
     }
 }
